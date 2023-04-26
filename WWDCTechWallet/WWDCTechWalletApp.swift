c873120fd53fa7d0e9be227f5c71162a94c666cb
Joao Gripp <corguinha@gmail.com>
2023-04-26 13:19:23 +0100

    Initial Commit

---
+++ WWDCTechWallet/WWDCTechWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCTechWalletApp.swift
+//  WWDCTechWallet
+//
+//  Created by Joao Gripp on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WWDCTechWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
